35f6d80c178d3c06cbe23ea959cea870628dd43f
Christof Stromberger <stromberger@student.tugraz.at>
2013-02-21 22:28:43 +0100

    Source format, lincense text

--- src/at/tugraz/iaik/io/MBUtils.java
+++ src/at/tugraz/iaik/io/MBUtils.java
@@ -20,26 +20,26 @@ import java.io.DataInputStream;
 import java.io.IOException;
 
 /**
- *
+ * 
  * @author MikeUCFL
  */
 public class MBUtils {
-    
-    public static int offset = 0;
-    
-    public static String getString(DataInputStream dis) throws IOException {
-        String value = null;
-            
-        int length = dis.readUnsignedShort();
-        offset += 2;
-        if (length > 0 && length < 65535) {
-            byte[] data = new byte[length];
 
-            dis.read(data);
-            value = new String(data);
-            offset += length;
-        }
-            
-        return value;
-    }
+	public static int offset = 0;
+
+	public static String getString(DataInputStream dis) throws IOException {
+		String value = null;
+
+		int length = dis.readUnsignedShort();
+		offset += 2;
+		if (length > 0 && length < 65535) {
+			byte[] data = new byte[length];
+
+			dis.read(data);
+			value = new String(data);
+			offset += length;
+		}
+
+		return value;
+	}
 }
